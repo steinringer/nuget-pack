a78d49453ae8ca1a252d72232a5780a007a848c7
Jozef Lacny <jozef.lacny@steinringer.sk>
2016-07-12 12:18:08 +0200

    implemented add method

--- nuget-pckg.js
+++ nuget-pckg.js
@@ -1,95 +1,122 @@
 'use strict';
 var fs = require('fs'),
-    Nuget = require('nuget-runner'),
-    path = require('path'),
-    find = require('find');
+	Nuget = require('nuget-runner'),
+	path = require('path'),
+	find = require('find');
 
 var Nu = function (options) {
-    this.options = options;
+	this.options = options;
 }
 
-Nu.prototype.getNuspecs = function (callback) {
+var shouldSkip = function (options, nuspecFile) {
+	if (options.skip == null) options.skip = [];
+	options.skip.push('node_modules');
+	var should = false;
+	
+	options.skip.forEach(function (skip) {
+		var regex = new RegExp(skip);
+		should = should || regex.test(nuspecFile);
+	});
+	return should;
+}
+
+var resolveDir = function (self) {
+	var dir = self.options.baseDir;
+	if (!path.isAbsolute(self.options.baseDir)) {
+		dir = path.join(__dirname, self.options.baseDir);
+	}
+	return dir;
+}
+
+Nu.prototype.getNuspecs = function (callback, options) {
+	options = options || {};
+	
 	var self = this;
-    var dir = this.options.baseDir;
-    if (!fs.existsSync(dir))
-        return callback(new Error('Provided baseDir does not exist'), null);
-    
-    if (!path.isAbsolute(this.options.baseDir)) {
-        dir = path.join(__dirname, this.options.baseDir);
-    }
-    find.file(/\w\.nuspec$/, dir, function (files) {
-        if (callback && typeof callback === 'function') {
-            var filtered = files.filter(function (f) {
-		        return !shouldSkip(f);
-            });
-	        if (filtered.length == 0)
-                callback(new Error('No .nuspec files match the criteria'), null);
-            else
-                callback(null, filtered);
-        } else {
-            throw new Error('You have to provide callback function.');
-        }
+	var dir = resolveDir(self);
+	
+	if (!fs.existsSync(dir))
+		return callback(new Error('Provided baseDir does not exist'), null);
+	
+	find.file(/\w\.nuspec$/, dir, function (files) {
+		if (callback && typeof callback === 'function') {
+			var filtered = files.filter(function (f) {
+				return !shouldSkip(options, f);
+			});
+			if (filtered.length == 0)
+				callback(new Error('No .nuspec files match the criteria'), null);
+			else
+				callback(null, filtered);
+		} else {
+			throw new Error('You have to provide callback function.');
+		}
 		
-    });
-
-    var shouldSkip = function (nuspecFile) {
-        if (self.options.skip == null) self.options.skip = [];
-	    self.options.skip.push('node_modules');
-        var should = false;    
-        
-        self.options.skip.forEach(function (skip) {
-            var regex = new RegExp(skip);
-            should = should || regex.test(nuspecFile);
-        });
-
-	    
-	    return should;
-    }
-
+	});
+	return;
 };
 
-Nu.prototype.resolveOutputPath = function () {
-    var self = this;
-	if (self.options.outputPath)
-		return self.options.outputPath;
-
-    return path.join(__dirname, self.options.baseDir);
+Nu.prototype.getNupkgs = function (callback) {
+    
 }
 
-Nu.prototype.pack = function (callback) {
-    var self = this;
+Nu.prototype.resolveOutputPath = function (self, options) {
+	var outputDirectory = options.outputDirectory;
+	if (outputDirectory) {
+		return outputDirectory;
+	}
+	return path.join(__dirname, self.options.baseDir);
+}
 
-    var nugetExePath = path.join(__dirname, 'bin/nuget.exe');
-    
-    var nuget = Nuget({
-        nugetPath: nugetExePath,
-        verbosity: 'quiet'
-    });
-    
-    self.getNuspecs(function (error, res) {
-        var resolvedCnt = 0;
-        var expectedCnt = res.length;
+Nu.prototype.pack = function (callback, options) {
+	options = options || {};
+	var self = this;
+	
+	var nugetExePath = path.join(__dirname, 'bin/nuget.exe');
+	
+	var nuget = Nuget({
+		nugetPath: nugetExePath,
+		verbosity: 'quiet'
+	});
+	self.getNuspecs(function (error, res) {
+		var resolvedCnt = 0;
+		var expectedCnt = res.length;
+		var outputDir = self.resolveOutputPath(self, options);
+		if (!fs.existsSync(outputDir)) {
+			fs.mkdirSync(outputDir);
+		}
+		
+		res.forEach(function (item) {
+			
+			nuget.pack({
+				spec: item,
+				outputDirectory: outputDir
+			})
+                .then(function () {
+				resolvedCnt++;
+				if (resolvedCnt === expectedCnt && callback && typeof callback == 'function')
+					callback();
+			});
 
-        var outputDir = self.resolveOutputPath();
-        if (!fs.existsSync(outputDir)) {
-            fs.mkdirSync(outputDir);
-        }
 
-        res.forEach(function (item) {
-            
-	        nuget.pack({
-		        spec: item,
-		        outputDirectory: outputDir
-	        })
-            .then(function () {
-                resolvedCnt++;
-	            if (resolvedCnt == expectedCnt && callback && typeof callback == 'function')
-		            callback();
-            });
+		});
+	}, options);
+}
 
-
-        });
-    });
+Nu.prototype.add = function (callback, options) {
+	options = options || {};
+	var nugetExePath = path.join(__dirname, 'bin/nuget.exe');
+	var nuget = Nuget({
+		nugetPath: nugetExePath,
+		verbosity: 'quiet'
+	});
+	
+	nuget.add({
+		nupkg: options.nupkg,
+		source: options.source
+	}).then(function () {
+		if (callback && typeof callback === 'function') {
+			callback();
+		}
+	});
 }
 
 module.exports = Nu;
--- tests/index.spec.js
+++ tests/index.spec.js
@@ -1,128 +1,168 @@
 ﻿'use strict';
 describe('nuget-pckg', function () {
-    
-    var Ng = require('../nuget-pckg.js'),
-        fse = require('fs-extra'),
-        fs = require('fs');
-    
-    it('should throw if baseDir does not exist', function (done) {
-        //Arrange
-        var ng = new Ng({
-            baseDir: './aa'
-        });
-        
-        //Act
-        ng.getNuspecs(function (err, res) {
-            expect(err).toBeDefined();
-            expect(err.toString()).toBe('Error: Provided baseDir does not exist');
-            done();
-        });
+	
+	var Ng = require('../nuget-pckg.js'),
+		fse = require('fs-extra'),
+		fs = require('fs');
+	
+	describe('getNuspecs', function () {
+		it('should throw if baseDir does not exist', function (done) {
+			//Arrange
+			var ng = new Ng({
+				baseDir: './aa'
+			});
+			
+			//Act
+			ng.getNuspecs(function (err, res) {
+				expect(err).toBeDefined();
+				expect(err.toString()).toBe('Error: Provided baseDir does not exist');
+				done();
+			});
+		    //Assert
 
-		//Assert
+		});
+		
+		it('should list nuspec files', function (done) {
+			//Arrange
+			var ng = new Ng({
+				baseDir: './tests'
+			});
+			
+			//Act
+			ng.getNuspecs(function (err, res) {
+				
+				//Assert
+				expect(res).toEqual(
+					[
+						__dirname + '\\proj1\\proj1.nuspec',
+						__dirname + '\\proj2\\proj2.nuspec'
+					]
+				);
+				done();
+			});
 
-    });
-    
-    it('should list nuspec files', function (done) {
-        //Arrange
-        var ng = new Ng({
-            baseDir: './tests'
-        });
-        
-        //Act
-        ng.getNuspecs(function (err, res) {
-            
-            //Assert
-            expect(res).toEqual(
-                [
-                    __dirname + '\\proj1\\proj1.nuspec',
-                    __dirname + '\\proj2\\proj2.nuspec'
-                ]
-            );
-            done();
-        });
+		});
+		
+		it('should list filtered nuspec files', function (done) {
+			//Arrange
+			var ng = new Ng({
+				baseDir: './tests'
+			});
+			
+			//Act
+			ng.getNuspecs(function (err, res) {
+				//Assert
+				expect(res).toEqual(
+					[
+						__dirname + '\\proj1\\proj1.nuspec'
+					]
+				);
+				done();
+			},
+			{ skip: ['proj2*.nuspec'] });
 
-    });
+		});
+		
+		it('should callback with error if all nuspec files filtered out', function (done) {
+			//Arrange
+			var ng = new Ng({
+				baseDir: './tests'
+			});
+			
+			//Act
+			ng.getNuspecs(function (err, res) {
+				//Assert
+				expect(err).toBeDefined();
+				expect(err.toString()).toBe('Error: No .nuspec files match the criteria');
+				expect(res).toBeNull();
+				done();
+			},
+			{ skip: ['proj1', 'proj2'] });
 
-	it('should list filtered nuspec files', function(done) {
-		//Arrange
-        var ng = new Ng({
-            baseDir: './tests',
-            skip: ['proj2*.nuspec']
-        });
-
-		//Act
-        ng.getNuspecs(function (err, res) {
-            //Assert
-            expect(res).toEqual(
-                [
-                    __dirname + '\\proj1\\proj1.nuspec'
-                ]
-            );
-            done();
-        });
-
-    });
-
-    it('should callback with error if all nuspec files filtered out', function(done) {
-        //Arrange
-        var ng = new Ng({
-            baseDir: './tests',
-            skip: ['proj1', 'proj2']
-        });
-        
-        //Act
-        ng.getNuspecs(function (err, res) {
-            //Assert
-            expect(err).toBeDefined();
-	        expect(err.toString()).toBe('Error: No .nuspec files match the criteria');
-	        expect(res).toBeNull();
-            done();
-        });
-
+		});
 	});
-    
-    it('should create nuget package with specified nuspec file', function (done) {
-        //Arrange
-        var expectedPackage = './tests/publishFolder/Proj1.1.0.0.nupkg';
-        
-        var ng = new Ng({
-            baseDir: './tests',
-            outputPath: './tests/publishFolder',
-            skip: ['proj2']
-        });
-        
-        //Act
-        ng.pack(function () {
-            //Assert
-	        var created = fs.existsSync(expectedPackage);
-            expect(created).toBeTruthy();
-            clean(expectedPackage);
-            done();
-        });
-    });
-
-	it('should create nuget package without outputpath specified', function(done) {
-		//Arrange
-        var expectedPackage = './tests/Proj1.1.0.0.nupkg';
-
-        var ng = new Ng({
-            baseDir: './tests',
-            skip: ['proj2.nuspec']
-        });
-
-		//Act
-        ng.pack(function () {
-            //Assert
-            var created = fs.existsSync(expectedPackage);
-            expect(created).toBeTruthy();
-            clean(expectedPackage);
-            done();
-        });
-
-
+	
+	describe('pack', function () {
+		it('should create nuget package with specified nuspec file', function (done) {
+			//Arrange
+			var expectedPackage = './tests/publishFolder/Proj1.1.0.0.nupkg';
+			
+			var ng = new Ng({
+				baseDir: './tests'
+			});
+			
+			//Act
+			ng.pack(function () {
+				//Assert
+				var created = fs.existsSync(expectedPackage);
+				expect(created).toBeTruthy();
+				done();
+				clean(expectedPackage);
+			},
+		    {
+				outputDirectory: './tests/publishFolder',
+				skip: ['proj2']
+			});
+		});
+		
+		it('should create nuget package without outputpath specified', function (done) {
+			//Arrange
+			var expectedPackage = './tests/Proj1.1.0.0.nupkg';
+			
+			var ng = new Ng({
+				baseDir: './tests'
+			});
+			
+			//Act
+			ng.pack(function () {
+				//Assert
+				var created = fs.existsSync(expectedPackage);
+				expect(created).toBeTruthy();
+				done();
+				clean(expectedPackage);
+			},
+		    {
+				skip: ['proj2.nuspec']
+			});
+		});
 	});
-    
-    var clean = function (expectedPackage) {
-        fse.removeSync(expectedPackage);
-    }
+	
+	describe('add', function () {
+		
+		it('should add single nuget package to folder structure', function (done) {
+			//Arrange
+			var expectedPackage = [
+				'./tests/server/proj1/1.0.0/proj1.1.0.0.nupkg',
+				'./tests/server/proj1/1.0.0/proj1.1.0.0.nupkg.sha512',
+				'./tests/server/proj1/1.0.0/proj1.nuspec'
+			];
+
+			var ng = new Ng({
+				baseDir: './tests'
+			});
+
+		    //Act
+			ng.add(function () {
+				
+				expectedPackage.forEach(function(e) {
+					var created = fs.existsSync(e);
+					expect(created).toBeTruthy();
+					clean(e);
+				});
+				done();
+		    },
+		    {
+				nupkg: './tests/packed/Proj1.1.0.0.nupkg',
+				source: './tests/server/'
+		    });
+
+		    //Assert
+
+		});
+		
+	});
+	
+	var clean = function (expectedPackage) {
+		fse.removeSync(expectedPackage);
+	}
 });
